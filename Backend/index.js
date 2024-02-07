d5a590c113bd83f2e2b89a4e6d9fc7dfca98d45e
Manish Kandukuri <114024130+Manishkandukuri-git@users.noreply.github.com>
2024-02-07 10:34:24 -0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,6 +13,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use("/user", router);
 
+
 app.listen(PORT, () => {
   console.log("Server listening on ", PORT);
 });
