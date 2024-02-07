bce70cd3bcf0c87efccd9b4423c9b33ba71bb3d6
Manish Kandukuri <114024130+Manishkandukuri-git@users.noreply.github.com>
2024-02-07 10:33:17 -0500

    Update bcrypt.js

--- Backend/utils/bcrypt.js
+++ Backend/utils/bcrypt.js
@@ -4,6 +4,7 @@ export const encryptPassword = (password) => {
   return new Promise((resolve, reject) => {
     bcrypt.genSalt(parseInt(process.env.SALT_ROUNDS), (err, salt) => {
       bcrypt.hash(password, salt, function (err, hash) {
+        
         resolve(hash);
       });
     });
